f2cdcbfcf094c2adb79d411052c9eb58bbe1182e
Giulliano Gazzoni <giugazz@gmail.com>
2021-06-29 18:31:14 -0300

    Service layer, component registration

---
+++ src/main/java/com/giullianogazzoni/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.giullianogazzoni.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.giullianogazzoni.course.entities.User;
+import com.giullianogazzoni.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
